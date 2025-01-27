659c769483f8ceb44ef20ade64bec092790df85e
Bahman026 <Bahman026@gmail.com>
2025-01-27 21:48:38 +0330

    feat(service-provider): register consultant repository- Bind ConsultantRepositoryInterface to ConsultantRepository in AppServiceProvider - Add ConsultantRepository and ConsultantRepositoryInterface to App\Providers namespace

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace App\Providers;
 
 use App\Repositories\ClientRepository;
+use App\Repositories\ConsultantRepository;
 use App\Repositories\Interfaces\ClientRepositoryInterface;
+use App\Repositories\Interfaces\ConsultantRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -16,6 +18,7 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->bind(ClientRepositoryInterface::class, ClientRepository::class);
+        $this->app->bind(ConsultantRepositoryInterface::class, ConsultantRepository::class);
     }
 
     /**
