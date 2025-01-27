4fbe3c1f02e2068c8f23a3157e2156086ca4f1c4
Bahman026 <Bahman026@gmail.com>
2025-01-27 22:10:16 +0330

    feat(repositories): add AppointmentRepository and interface
    - Create AppointmentRepository class implementing

---
+++ app/Repositories/AppointmentRepository.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repositories;
+
+use App\Models\Client;
+use App\Repositories\Interfaces\AppointmentRepositoryInterface;
+use Illuminate\Database\Eloquent\Collection;
+
+class AppointmentRepository implements AppointmentRepositoryInterface
+{
+    public function create($data)
+    {
+        return Client::query()->create($data);
+    }
+
+    public function update(array $data, $id)
+    {
+        $user = Client::query()->findOrFail($id);
+        $user->update($data);
+
+        return $user;
+    }
+
+    public function delete($id): ?bool
+    {
+        $user = Client::query()->findOrFail($id);
+
+        return $user->delete();
+    }
+
+    public function all(): Collection
+    {
+        return Client::all();
+    }
+
+    public function find($id)
+    {
+        return Client::query()->findOrFail($id);
+    }
+}
