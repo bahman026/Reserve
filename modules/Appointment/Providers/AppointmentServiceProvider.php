af29518fab4fafe9d1f3787f60929eaaa1cb6a63
Bahman026 <Bahman026@gmail.com>
2025-01-28 18:13:08 +0330

    feat(Appointment): create service provider and set up module routes
    - Create AppointmentServiceProvider to load migrations and register other service providers
    - Add RepositoryServiceProvider to bind AppointmentRepositoryInterface to AppointmentRepository
    - Implement RouteServiceProvider to define API routes for appointments
    - Create routes.php file with CRUD routes for appointments

---
+++ modules/Appointment/Providers/AppointmentServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Appointment\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppointmentServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
+        $this->app->register(RouteServiceProvider::class);
+        $this->app->register(RepositoryServiceProvider::class);
+
+    }
+}
