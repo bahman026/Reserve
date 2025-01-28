d63890b21f1231b801aeccaac611cef810e7974b
Bahman026 <Bahman026@gmail.com>
2025-01-28 13:28:45 +0330

    feat(appointment): add method to get all appointments with relationships
    - Add getAllAppointments method to AppointmentRepository
    - Include eager loading of consultant and client relationships

--- app/Repositories/Interfaces/AppointmentRepositoryInterface.php
+++ app/Repositories/Interfaces/AppointmentRepositoryInterface.php
@@ -4,4 +4,9 @@ declare(strict_types=1);
 
 namespace App\Repositories\Interfaces;
 
-interface AppointmentRepositoryInterface extends RepositoryInterface {}
+use Illuminate\Database\Eloquent\Collection;
+
+interface AppointmentRepositoryInterface extends RepositoryInterface
+{
+    public function getAllAppointments(): Collection;
+}
