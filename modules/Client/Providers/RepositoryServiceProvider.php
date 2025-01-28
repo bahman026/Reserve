3deb425218e4bfff3b0fdd1598b0d666a1db4b26
Bahman026 <Bahman026@gmail.com>
2025-01-28 17:58:13 +0330

    feat(Client): create service providers for Client module
    - Create ClientServiceProvider to load migrations and register other providers
    - Add RepositoryServiceProvider to bind repository interfaces
    - Implement RouteServiceProvider to define API routes for the Client module

---
+++ modules/Client/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Client\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Modules\Consultant\Repositories\ConsultantRepository;
+use Modules\Consultant\Repositories\Interfaces\ConsultantRepositoryInterface;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        $this->app->bind(ConsultantRepositoryInterface::class, ConsultantRepository::class);
+    }
+
+    public function boot(): void
+    {
+        //
+    }
+}
