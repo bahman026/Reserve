c85240d74936d700f7eb7f5555e83b731c0b0b4a
Bahman026 <Bahman026@gmail.com>
2025-01-28 17:58:57 +0330

    feat(bootstrap): add ClientServiceProvider to providers list
    - Include ClientServiceProvider from Modules\Client\Providers
    - Add ClientServiceProvider to the list of providers in bootstrap/providers.php

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,10 +2,12 @@
 
 declare(strict_types=1);
 
+use Modules\Client\Providers\ClientServiceProvider;
 use Modules\Consultant\Providers\ConsultantServiceProvider;
 
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\RepositoryServiceProvider::class,
     ConsultantServiceProvider::class,
+    ClientServiceProvider::class,
 ];
